755f5f329a4548016b3fb8895289082529b40452
nayeemtalukder1 <nayeemtalukder882@gmail.com>
2026-02-08 16:21:32 +0600

    new

--- shohanur/next.config.ts
+++ shohanur/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "backend-shohanur-1.onrender.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
